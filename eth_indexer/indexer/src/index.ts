09b93217d4121094195c95fa6d13d006ace73860
Sarfraz Alam <sarfrazk7858@gmail.com>
2025-02-22 06:57:25 +0530

    indexer

---
+++ eth_indexer/indexer/src/index.ts
@@ -0,0 +1,10 @@
+let CURRENT_BLOCK_NUMBER = 1213123;
+
+async function main(){
+    // get the interested addresses from the DB
+
+
+    // Inspect the block for native eth transactions on one of these addresses
+
+    // bad approach => update the balance in the database
+}
