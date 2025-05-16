553918cca00e389f7de78e060e634e15d190390c
Sarfraz Alam <sarfrazk7858@gmail.com>
2025-05-16 15:33:58 +0530

    tooken launch pad

--- decentralized_app/src/Airdrop.tsx
+++ decentralized_app/src/Airdrop.tsx
@@ -1,19 +1,21 @@
 import { useConnection, useWallet } from "@solana/wallet-adapter-react"
 import { PublicKey } from "@solana/web3.js";
+import { useState } from "react";
 
 export default function Airdrop() {
     const wallet = useWallet()
     const { connection } = useConnection()
+    const [num,setNum] = useState<string>('')
 
     async function sendAirdropUser() {
-        await connection.requestAirdrop(wallet.publicKey, 1000000000)
+        await connection.requestAirdrop(wallet.publicKey as PublicKey, Number(num) * 1000000000)
         alert("hiii")
     }
 
     return (
         <div>
             you address {wallet.publicKey?.toString()}
-            <input type="text" placeholder="Amount"></input>
+            <input  type="text" placeholder="Amount"  onChange={e=>setNum(e.target.value)}></input>
             <button onClick={sendAirdropUser}>Send Airdrop</button>
         </div>
     )
---
+++ week-6-web3-token-launchpad/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
@@ -0,0 +1,89 @@
+import {
+  createInitializeAccount2Instruction,
+  getMinimumBalanceForRentExemptMint,
+  MINT_SIZE,
+} from "@solana/spl-token";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { Keypair } from "@solana/web3.js";
+import { useState } from "react";
+import { Transaction, SystemProgram } from "@solana/web3.js";
+
+export function TokenLaunchpad() {
+  const [name, setName] = useState("");
+  const [symbol, setSymbol] = useState("");
+  const [image, setImage] = useState("");
+  const [initialSupply, setInitialSupply] = useState("");
+
+  const { connection } = useConnection();
+  const wallet = useWallet();
+
+  async function createToken() {
+    const lamports = await getMinimumBalanceForRentExemptMint(connection);
+    const keypair = Keypair.generate();
+
+    const transaction = new Transaction().add(
+      SystemProgram.createAccount({
+        fromPubkey: payer.publicKey,
+        newAccountPubkey: keypair.publicKey,
+        space: MINT_SIZE,
+        lamports,
+        programId,
+      }),
+      createInitializeAccount2Instruction(
+        keypair.publicKey,
+        decimals,
+        mintAuthority,
+        freezeAuthority,
+        programId
+      )
+    );
+
+    transaction.partialSign(keypair);
+    await wallet.signTransaction(transaction);
+  }
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h1>Solana Token Launchpad</h1>
+      <input
+        className="inputText"
+        type="text"
+        placeholder="Name"
+        onChange={(e) => setName(e.target.value)}
+      ></input>{" "}
+      <br />
+      <input
+        className="inputText"
+        type="text"
+        placeholder="Symbol"
+        onChange={(e) => setSymbol(e.target.value)}
+      ></input>{" "}
+      <br />
+      <input
+        className="inputText"
+        type="text"
+        placeholder="Image URL"
+        onChange={(e) => setImage(e.target.value)}
+      ></input>{" "}
+      <br />
+      <input
+        className="inputText"
+        type="text"
+        placeholder="Initial Supply"
+        onChange={(e) => setInitialSupply(e.target.value)}
+      ></input>{" "}
+      <br />
+      <button className="btn" onClick={createToken}>
+        Create a token
+      </button>
+    </div>
+  );
+}
