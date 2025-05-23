6664fa40e2a95711b48c1cbbbc5970df5f96302c
Sarfraz Alam <sarfrazk7858@gmail.com>
2025-05-23 15:08:58 +0530

    wagmi

--- wagmi/src/App.jsx
+++ wagmi/src/App.jsx
@@ -1,8 +1,80 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  http,
+  createConfig,
+  useConnect,
+  WagmiProvider,
+  useAccount,
+  useBalance,
+  useSendTransaction,
+} from "wagmi";
+import { base, mainnet, optimism } from "wagmi/chains";
+import { injected, metaMask, safe, walletConnect } from "wagmi/connectors";
+
+const queryClient = new QueryClient();
+
+export const config = createConfig({
+  chains: [mainnet],
+  connectors: [injected()],
+  transports: {
+    [mainnet.id]: http(),
+  },
+});
+
 function App() {
-  return <div>
-    <input type="text" placeholder="Address..." />
-    <button>Send 0.1 ETH</button>
-  </div>;
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <WalletConnector />
+        <EthSend />
+        <MyAddress />
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+function MyAddress() {
+  const { address } = useAccount();
+  const balance = useBalance({ address });
+
+  return (
+    <div>
+      {address} <br />
+      {balance?.data?.formatted}
+    </div>
+  );
+}
+
+function WalletConnector() {
+  const { connectors, connect } = useConnect();
+  return (
+    <div>
+      {connectors.map((connector) => (
+        <button key={connector.uid} onClick={() => connect({ connector })}>
+          {connector.name}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+function EthSend() {
+  const { data: hash, sendTransaction } = useSendTransaction();
+
+  function sendEth() {
+    sendTransaction({
+      to: document.getElementById("addr").value,
+      value: "100000000000000000",
+    });
+  }
+
+  return (
+    <div>
+      <input type="text" id="addr" placeholder="Address..." />
+      <button onClick={sendEth}>Send 0.1 ETH</button>
+      {hash}
+    </div>
+  );
 }
 
 export default App;
